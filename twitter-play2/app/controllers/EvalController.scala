bde060e431b22b6b3ac7fb4d342b73e46aa701ee
ap8322 <yuki.haneda@bizreach.co.jp>
2016-08-18 13:50:44 +0900

    eval toggle done

---
+++ twitter-play2/app/controllers/EvalController.scala
@@ -0,0 +1,106 @@
+package controllers
+
+
+import com.google.inject.Inject
+import controllers.EvalController.tweetIdForm
+import jp.t2v.lab.play2.auth.AuthElement
+import models.Tables._
+import models._
+import play.api.libs.json._
+import play.api.mvc.Controller
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+/**
+  * Created by yuki.haneda on 2016/08/18.
+  */
+class EvalController @Inject()(val memberDAO: MemberDAO,
+                               val tweetDAO: TweetDAO,
+                               val evalDAO: EvalDAO)
+  extends Controller with AuthElement with AuthConfigImpl {
+  /**
+    * iine!
+    *
+    * @return
+    */
+  def good() = AsyncStack(parse.json, AuthorityKey -> None) { implicit rs =>
+    rs.body.validate[tweetIdForm].map { form =>
+      //そのコメント(tweetId)に対して､その人(loggedIn.id)がいいねした事を登録
+      val eval = EvalRow(form.tweetId, form.evalStatus, loggedIn.memberId)
+      evalDAO.insertStatus(eval)
+      Future.successful(Ok(Json.obj("result" -> form.tweetId, "eval_status" -> form.evalStatus)))
+    }.recoverTotal { e =>
+      Future {
+        Ok(Json.obj("result" -> "failure", "error" -> JsError.toJson(e)))
+      }
+    }
+  }
+
+  /**
+    * None
+    *
+    * @return
+    */
+  def deleteEval() = AsyncStack(parse.json, AuthorityKey -> None) { implicit rs =>
+    rs.body.validate[tweetIdForm].map { form =>
+      evalDAO.deleteStatus(form.tweetId, loggedIn.memberId)
+      Future.successful(Ok(Json.obj("result" -> form.tweetId, "eval_status" -> form.evalStatus)))
+    }.recoverTotal { e =>
+      Future {
+        Ok(Json.obj("result" -> "failure", "error" -> JsError.toJson(e)))
+      }
+    }
+  }
+
+  /**
+    * doudemoiine!
+    *
+    * @return
+    */
+  def bad() = AsyncStack(parse.json, AuthorityKey -> None) { implicit rs =>
+    rs.body.validate[tweetIdForm].map { form =>
+      //そのコメント(tweetId)に対して､その人(loggedIn.id)がいいねした事を登録
+      val eval = EvalRow(form.tweetId, 0, loggedIn.memberId)
+      evalDAO.insertStatus(eval)
+      Future.successful(Ok(Json.obj("result" -> form.tweetId, "eval_status" -> form.evalStatus)))
+    }.recoverTotal { e =>
+      Future {
+        Ok(Json.obj("result" -> "failure", "error" -> JsError.toJson(e)))
+      }
+    }
+  }
+
+  /**
+    * update status
+    *
+    * @return
+    */
+  def updateStatus() = AsyncStack(parse.json, AuthorityKey -> None) { implicit rs =>
+    rs.body.validate[tweetIdForm].map { form =>
+      //そのコメント(tweetId)に対して､その人(loggedIn.id)がいいねした事を登録
+      val eval = EvalRow(form.tweetId, form.evalStatus, loggedIn.memberId)
+      evalDAO.updateStatus(eval)
+      Future.successful(Ok(Json.obj("result" -> form.tweetId, "eval_status" -> form.evalStatus)))
+    }.recoverTotal { e =>
+      Future {
+        Ok(Json.obj("result" -> "failure", "error" -> JsError.toJson(e)))
+      }
+    }
+  }
+}
+
+object EvalController {
+
+  case class tweetIdForm(tweetId: Int, evalStatus: Int)
+
+  implicit val tweetFormFormat: Reads[tweetIdForm] = new Reads[tweetIdForm] {
+    override def reads(json: JsValue): JsResult[tweetIdForm] = {
+      JsSuccess(tweetIdForm(
+        tweetId = (json \ "tweet_id").as[Int],
+        evalStatus = (json \ "eval_status").as[Int]
+      )
+      )
+    }
+  }
+}
--- twitter-play2/public/javascripts/hello.js
+++ twitter-play2/public/javascripts/hello.js
@@ -4,7 +4,7 @@ function follow(id) {
         type: 'POST',
         data: '{"id": ' + id + '}',
         contentType: 'application/json',
-        dataType: 'json',
+        dataType: 'json'
     }).done(function (data) {
         console.log(data);
         $('#member' + id).html("<button onclick='remove(" + id + ")'>フォロー中</button>");
@@ -24,3 +24,114 @@ function remove(id) {
         $('#member' + id).html("<button onclick='follow(" + id + ")'>フォロー</button>");
     })
 }
+
+$(function () {
+    $('.good').click(function () {
+        var button = $(this);
+        var count = $(this).next();
+        var tweet_id = count.next().next().next().text();
+        var next = button.next().next();
+
+        //Non -> good
+        if (button.val() == 0) {
+            if (next.val() == 0) {
+                $.ajax({
+                    url: '/good',
+                    type: 'POST',
+                    data: '{"tweet_id":' + tweet_id + ', "eval_status":1}',
+                    contentType: 'application/json',
+                    dataType: 'json'
+                }).done(function () {
+                    button.text("いいねしました");
+                    button.val('1');
+                    count.text(parseInt(count.text()) + 1);
+                });
+
+                //bad -> good
+            } else {
+                $.ajax({
+                    url: '/updateStatus',
+                    type: 'PUT',
+                    data: '{"tweet_id":' + tweet_id + ', "eval_status":1}',
+                    contentType: 'application/json',
+                    dataType: 'json'
+                }).done(function () {
+                    button.text("いいねしました");
+                    button.val('1');
+                    count.text(parseInt(count.text()) + 1);
+                    next.text("どうでもいいね");
+                    next.val(0);
+                    next.next().text(parseInt(next.next().text()) - 1)
+                })
+            }
+
+            //good -> Non
+        } else {
+            $.ajax({
+                url: '/deleteEval',
+                type: 'DELETE',
+                data: '{"tweet_id":' + tweet_id + ', "eval_status":10}',
+                contentType: 'application/json',
+                dataType: 'json'
+            }).done(function () {
+                button.text("いいね");
+                button.val(0);
+                count.text(parseInt(count.text()) - 1);
+            })
+        }
+    });
+
+    $('.bad').click(function () {
+        var button = $(this);
+        var count = $(this).next();
+        var prev = $(this).prev().prev();
+        var tweet_id = count.next().text();
+
+        //non -> bad
+        if (button.val() == 0) {
+            if (prev.val() == 0) {
+                $.ajax({
+                    url: '/bad',
+                    type: 'POST',
+                    data: '{"tweet_id":' + tweet_id + ', "eval_status":0}',
+                    contentType: 'application/json',
+                    dataType: 'json'
+                }).done(function () {
+                    button.text("どうでもいいねしました");
+                    button.val(1);
+                    count.text(parseInt(count.text()) + 1);
+                });
+
+                //good -> bad
+            } else {
+                $.ajax({
+                    url: '/updateStatus',
+                    type: 'PUT',
+                    data: '{"tweet_id":' + tweet_id + ', "eval_status":0}',
+                    contentType: 'application/json',
+                    dataType: 'json'
+                }).done(function () {
+                    button.text("どうでもいいねしました");
+                    button.val(1);
+                    count.text(parseInt(count.text()) + 1);
+                    prev.text("いいね");
+                    prev.val(0);
+                    prev.next().text(parseInt(prev.next().text()) - 1)
+                });
+            }
+            //bad -> none
+        } else {
+            $.ajax({
+                url: '/deleteEval',
+                type: 'DELETE',
+                data: '{"tweet_id":' + tweet_id + ', "eval_status":10}',
+                contentType: 'application/json',
+                dataType: 'json'
+            }).done(function () {
+                button.text("どうでもいいね");
+                button.val(0);
+                count.text(parseInt(count.text()) - 1);
+            })
+        }
+    })
+});
